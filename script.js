a3ee3ef299e1152807d8eab3ea5506113f34a156
Miguel <miguelnavas933@gmail.com>
2023-05-02 16:12:55 -0500

    Script para Nosotros.html el cual despliega y oculta las descripciones Mision, vision, sistema.

--- script.js
+++ script.js
@@ -1,10 +1,48 @@
-const icono = document.getElementById("icono");
-const contenido = document.getElementById("contenido");
+const iconos = document.querySelectorAll(".unfold");
 
-icono.addEventListener("click", function() {
-  if (contenido.style.display === "none") {
-    contenido.style.display = "block";
-  } else {
-    contenido.style.display = "none";
-  }
+iconos.forEach((icono) => {
+  icono.addEventListener("click", () => {
+    const descripcion = icono.parentNode.parentNode.nextElementSibling;
+    const descripciones = document.querySelectorAll(".description");
+    
+    descripciones.forEach((desc) => {
+      if (desc !== descripcion && desc.classList.contains("descripcion-comun")) {
+        desc.style.display = "none";
+      }
+    });
+    
+    if (descripcion.classList.contains("descripcion-comun")) {
+      descripcion.style.display = "block";
+    }
+  });
 });
+
+
+
+                          /*Despliega la descripcion de sistemas.*/ 
+function toggleSistema() {
+var icono = document.getElementById("sistema");
+var descripcion = document.getElementById("descripcion_sistema");
+var descripcionMision = document.getElementById("descripcion_mision");
+var descripcionVision = document.getElementById("descripcion_vision");
+
+// Cerrar otras descripciones si están abiertas
+if (descripcionMision.style.display === "block") {
+  toggleMision();
+}
+if (descripcionVision.style.display === "block") {
+  toggleVision();
+}
+
+if (icono.getAttribute("name") === "chevron-up-outline") {
+  icono.setAttribute("name", "chevron-down-outline");
+  descripcion.style.display = "none";
+  descripcion.style.height = "0";
+  document.querySelector('.SistemaIntegrado').style.height = "100px"; // Establece la altura del div a 100px
+} else {
+  icono.setAttribute("name", "chevron-up-outline");
+  descripcion.style.display = "block";
+  descripcion.style.height = descripcion.scrollHeight + "px";
+  document.querySelector('.SistemaIntegrado').style.height = descripcion.scrollHeight + 100 + "px"; // Ajusta la altura del div a la altura de la descripción más 100px
+}
+}   
