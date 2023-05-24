e8c8347f1031e0f24f79740e1fc1927026504c17
Miguel <miguelnavas933@gmail.com>
2023-05-24 17:29:26 -0500

    updated version

--- html_web/script_mision-vision.js
+++ html_web/script_mision-vision.js
@@ -1,110 +1,19 @@
+function toggleDescripcion(descripcionId) {
+  const descripcionElement = document.getElementById(descripcionId);
 
+  // Cerrar todas las descripciones
+  const descriptionElements = document.querySelectorAll('.description');
+  descriptionElements.forEach((description) => {
+    description.style.display = 'none';
+  });
 
-
-function toggleMision() {
-  toggleDescripcion('mision');
-}
-
-function toggleVision() {
-  toggleDescripcion('vision');
-}
-
-function toggleSistema() {
-  toggleDescripcion('sistema');
+  // Mostrar la descripción correspondiente
+  descripcionElement.style.display = 'block';
 }
 
 function toggleMadgalenaMedio(){
   toggleDescripcion('')
 }
-
-/*Despliega la descripcion de mision.*/
-function toggleMision() {
-  var icono = document.getElementById("mision");
-  var descripcion = document.getElementById("descripcion_mision");
-  var descripcionVision = document.getElementById("descripcion_vision");
-  var descripcionSistema = document.getElementById("descripcion_sistema");
-
-  if (icono.getAttribute("name") === "chevron-up-outline") {
-    icono.setAttribute("name", "chevron-down-outline");
-    descripcion.style.display = "none";
-    descripcion.style.height = "0";
-    document.querySelector('.mision').style.height = "150px";
-  } else {
-    icono.setAttribute("name", "chevron-up-outline");
-    descripcion.style.display = "block";
-    descripcion.style.height = "100%";
-    document.querySelector('.mision').style.height = "350px";
-  }
-
-  // Cerrar otras descripciones si están abiertas
-  if (descripcionVision.style.display === "block" && icono.getAttribute("name") === "chevron-up-outline") {
-    toggleVision();
-  }
-  if (descripcionSistema.style.display === "block" && icono.getAttribute("name") === "chevron-up-outline") {
-    toggleSistema();
-  }
-}
-
-
-
-/*Despliega la descripcion de vision.*/ 
-function toggleVision() {
-var icono = document.getElementById("vision");
-var descripcion = document.getElementById("descripcion_vision");
-var descripcionMision = document.getElementById("descripcion_mision");
-var descripcionSistema = document.getElementById("descripcion_sistema");
-
-// Cerrar otras descripciones si están abiertas
-if (descripcionMision.style.display === "block") {
-  toggleMision();
-}
-if (descripcionSistema.style.display === "block") {
-  toggleSistema();
-}
-
-if (icono.getAttribute("name") === "chevron-up-outline") {
-  icono.setAttribute("name", "chevron-down-outline");
-  descripcion.style.display = "none"; // Oculta la descripción
-  descripcion.style.height = "0"; // Establece la altura a 0px
-  document.querySelector('.vision').style.height = "120px"; // Establece la altura del div vision a 120px
-} else {
-  icono.setAttribute("name", "chevron-up-outline");
-  descripcion.style.display = "block"; // Muestra la descripción
-  descripcion.style.height = descripcion.scrollHeight + "px"; // Establece la altura dinámicamente
-  document.querySelector('.vision').style.height = "100%"; // Establece la altura del div vision a 350px
-}
-}
-
-
-/*Despliega la descripcion de sistemas.*/ 
-function toggleSistema() {
-var icono = document.getElementById("sistema");
-var descripcion = document.getElementById("descripcion_sistema");
-var descripcionMision = document.getElementById("descripcion_mision");
-var descripcionVision = document.getElementById("descripcion_vision");
-
-// Cerrar otras descripciones si están abiertas
-if (descripcionMision.style.display === "block") {
-  toggleMision();
-}
-if (descripcionVision.style.display === "block") {
-  toggleVision();
-}
-
-if (icono.getAttribute("name") === "chevron-up-outline") {
-  icono.setAttribute("name", "chevron-down-outline");
-  descripcion.style.display = "none";
-  descripcion.style.height = "0";
-  document.querySelector('.SistemaIntegrado').style.height = "120px"; // Establece la altura del div a 120px
-} else {
-  icono.setAttribute("name", "chevron-up-outline");
-  descripcion.style.display = "block";
-  descripcion.style.height = descripcion.scrollHeight + "px";
-  document.querySelector('.SistemaIntegrado').style.height = descripcion.scrollHeight + 100 + "%"; // Ajusta la altura del div a la altura de la descripción más 100%
-}
-}   
-
-
 
 function mostrarContenidoZona_1() {
   var contenido = document.getElementById("contenido_Zona1");
